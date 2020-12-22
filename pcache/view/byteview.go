7227f05257352e8266093e7c74468b22e6b7bd59
lovesagiri <905943026@qq.com>
2020-12-22 16:53:33 +0800

    包调整

---
+++ pcache/view/byteview.go
@@ -0,0 +1,23 @@
+package view
+
+type ByteView struct {
+	b []byte
+}
+
+func (b *ByteView) Len() int {
+	return len(b.b)
+}
+
+func (b *ByteView) ByteSlice() []byte {
+	return cloneBytes(b.b)
+}
+
+func (b ByteView) String() string {
+	return string(b.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	newone := make([]byte, len(b))
+	copy(newone, b)
+	return newone
+}
